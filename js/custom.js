d2ef47159342c5075e3f41cd3b4d37ce26e3084d
neumanv <guillermoanv25@gmail.com>
2023-02-24 23:21:14 +0100

    En proceso

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,28 @@
 
 AOS.init(); //Inicia la libreria AOS
 
+//Responsive navbar
 function responsive() {
     let nav = document.getElementById("nav");
-    if(nav.className === "header"){
+    if (nav.className === "header") {
         nav.className += " responsive";
-    }else{
+    } else {
         nav.className = "header";
     }
 }
 
+//Desplegable de información
 $(document).ready(function () {
     $(".toggle").click(function () {
         $("#info-ubi").toggle();
+    });
+});
+
+//Hover de cartas
+$(document).ready(function () {
+    $(".carta").hover(function () {
+        $(this).css("background-color", "#c5c6c6");
+    }, function(){
+        $(".carta").css("background-color", "var(--principal)");
     });
 });
