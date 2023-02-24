522b15086d1eba4c80f1e424886a0154fe8e5715
neumanv <guillermoanv25@gmail.com>
2023-02-24 19:22:42 +0100

    Empezando enserio

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,18 @@
 "use strict"
 
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "header") {
-        x.className += " responsive";
-    } else {
-        x.className = "header";
+AOS.init(); //Inicia la libreria AOS
+
+function responsive() {
+    let nav = document.getElementById("nav");
+    if(nav.className === "header"){
+        nav.className += " responsive";
+    }else{
+        nav.className = "header";
     }
 }
+
+$(document).ready(function () {
+    $(".toggle").click(function () {
+        $("#info-ubi").toggle();
+    });
+});
